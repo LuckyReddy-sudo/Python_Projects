73b15de90256133dc2d3e653b4f8031d5b26009e
LuckyReddy-sudo <70487533+LuckyReddy-sudo@users.noreply.github.com>
2023-02-06 18:36:34 -0500

    added 2 programs

---
+++ Move_Zero.py
@@ -0,0 +1,9 @@
+# From a list of numbers,movee zero to the end of the list.
+
+list = [1, 0, 2, 0, 4, 6]
+
+for i in list:
+    if i == 0:
+        list.remove(i)
+        list.append(i)
+print(list)
